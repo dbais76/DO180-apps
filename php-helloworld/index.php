851d2cb0f42df5ce4ea7c9fb1ceb4f0b930d2962
Daleep Singh Bais <57488638+dbais76@users.noreply.github.com>
2021-02-19 10:44:39 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, World-NEW!!!!!!, HAVE A GREAT Service Mesh LEARNING!!!  php-IV version is " . PHP_VERSION . "\n";
+print "Hello, World-NEW!!!!!!, HAVE A GREAT Weekend. Hope you enjoyed OCP Training !!!  php-IV version is " . PHP_VERSION . "\n";
 ?>
