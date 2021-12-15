576e7ba3ac65dde9346ff21917d7b1b707966eda
Daleep Singh Bais <57488638+dbais76@users.noreply.github.com>
2021-12-15 15:23:31 +0530

    Update index.php

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,3 @@
 <?php
-print "Hello, Welcome to DO328.Service Mesh is Great!!!!!!, . !!  php-V version is " . PHP_VERSION . "\n";
+print "Hello, Welcome to DO180!!, . !!  php-V version is " . PHP_VERSION . "\n";
 ?>
