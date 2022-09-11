064d5a131ab4010f4cb453ee07ae62f0d5dcc267
Daleep Singh Bais <57488638+dbais76@users.noreply.github.com>
2022-09-11 15:29:21 +0530

    blue deployment

--- php-helloworld-new/index.php
+++ php-helloworld-new/index.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
 <body>
-<h1 style="color: green; font-size:48px">This is Green Deployment!</h1>
+<h1 style="color: blue; font-size:48px">This is Blue Deployment!</h1>
 <?php
 
 print "<br>";
 print "<br>";
-print "<font color=green size='4pt'>Demos running on <b>Red Hat OpenShift</b> PHP Builder Image with PHP version " . PHP_VERSION .  "\n"; 
+print "<font color=blue size='4pt'>Demos running on <b>Red Hat OpenShift</b> PHP Builder Image with PHP version " . PHP_VERSION .  "\n"; 
 ?>
 
 </body>
